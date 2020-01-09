9e64eaeb85bbf659ad37955bc29eb85f834cd13a
sunhao <sunhao@localhost.localdomain>
2020-01-09 06:42:27 +0000

    user

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/user/reg','User\UserController@reg');
+Route::post('/user/login','User\UserController@login');
+Route::post('/user/getuserinfo','User\UserController@getuserinfo');
+Route::post('/user/gettoken','User\UserController@gettoken');
